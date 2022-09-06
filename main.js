337e3bd716a7e2a7983c9495d6ab46e04f1c74dd
MDSCSI0\mark.ladia <mbladia10@gmail.com>
2022-09-06 13:22:58 +0800

    initial

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
-
-
-
-
 const message = document.querySelector('.message');
 const ourImg = document.querySelector('#our-img');
+const loading = document.querySelector('#loading');
 
 ourImg.addEventListener("click", function(){
+  loading.style.display = "inline"
     const randNum = Math.floor(Math.random() * 1643);
     fetch("https://type.fit/api/quotes")
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
-      console.log(data);
-      console.log(data[randNum]);
       message.innerHTML = data[randNum].text;
     });
 })
